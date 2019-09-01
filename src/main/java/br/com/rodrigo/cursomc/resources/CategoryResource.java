7326fdc3db2c57948196ee5af5ce2022e69fd4b1
tigelah <lokoportitimao2009@hotmail.com>
2019-09-01 08:02:58 -0300

    Testando Classe Dominio Category

--- src/main/java/br/com/rodrigo/cursomc/resources/CategoryResource.java
+++ src/main/java/br/com/rodrigo/cursomc/resources/CategoryResource.java
@@ -1,8 +1,13 @@
 package br.com.rodrigo.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.rodrigo.cursomc.domain.Category;
 
 @RestController
 @RequestMapping(value="/category")
@@ -10,7 +15,15 @@ public class CategoryResource {
 	
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String list() {
-		return "REST func";
+	public List<Category> list() {
+		Category cat1 = new Category(1,"Informatica");
+		Category cat2 = new Category(2,"Escritorio");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		
+		return list;
 	}
 }

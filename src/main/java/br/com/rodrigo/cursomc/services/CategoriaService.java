e1e119e48a3211c6f1e9fec1dc27b6bc7068560a
tigelah <lokoportitimao2009@hotmail.com>
2019-09-15 14:56:00 -0300

    Adicionando o metodo DELETE Categorias

--- src/main/java/br/com/rodrigo/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/rodrigo/cursomc/services/CategoriaService.java
@@ -3,6 +3,7 @@ package br.com.rodrigo.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.rodrigo.cursomc.domain.Categoria;
@@ -20,14 +21,23 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id){
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityViolationException("Não é Possivel excluir uma categoria que contem produtos");
+		} 
 	}
 }
